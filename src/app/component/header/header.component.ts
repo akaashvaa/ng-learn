3d33c0bf018cdb77bd8f87eb82436dd5566b7bdc
AkashKumar-0 <ahmvaad@gmail.com>
2024-04-05 08:32:28 +0530

    first commit

---
+++ src/app/component/header/header.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [],
+  template: `
+    <h1>
+      header works!
+    </h1>
+  `,
+  styles: ``
+})
+export class HeaderComponent {
+
+}
