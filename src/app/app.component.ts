b94209c1de7065be81af73a5e15580368c410273
AkashKumar-0 <ahmvaad@gmail.com>
2024-04-11 19:38:24 +0530

    added ngModel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {HeaderComponent} from './component/header/header.component'
+import {FormsModule} from '@angular/forms'
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,  HeaderComponent],
+  imports: [RouterOutlet,FormsModule,HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'ng-learn';
   disable=true
+  twoway : String = "hello"
   handleInput(name : string){
     console.log(name)
   }
