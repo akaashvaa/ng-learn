174271554bf44a67cca432bb6624f5813b5a0af0
AkashKumar-0 <ahmvaad@gmail.com>
2024-04-13 16:05:35 +0530

    bug fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {FormsModule} from '@angular/forms'
-import { Assignment3Component } from './assignment3/assignment3.component'
-
-import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Assignment3Component } from './assignment3/assignment3.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,FormsModule, CommonModule, Assignment3Component],
+  imports: [RouterOutlet, FormsModule, Assignment3Component],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'ng-learn';
- }
+}
--- src/app/assignment3/assignment3.component.ts
+++ src/app/assignment3/assignment3.component.ts
@@ -1,15 +1,16 @@
 import { Component } from '@angular/core';
 
+import { NgClass } from '@angular/common';
+
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-assignment3',
   standalone: true,
-  imports: [CommonModule],
+  imports: [NgClass],
   templateUrl: './assignment3.component.html',
-  styleUrl: './assignment3.component.scss'
+  styleUrl: './assignment3.component.scss',
 })
-
 export class Assignment3Component {
   toggleParagraph: boolean = true;
   id: number = 0;
@@ -18,8 +19,6 @@ export class Assignment3Component {
   toggleDisplayDetails() {
     this.queue.push({ id: this.id++, date: new Date() });
     this.toggleParagraph = !this.toggleParagraph;
-    console.log(this.id)
+    console.log(this.id);
   }
-
 }
-
