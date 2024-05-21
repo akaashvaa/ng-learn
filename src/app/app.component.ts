8bfb22652827e3a8495168352066da7679f34361
AkashKumar-0 <ahmvaad@gmail.com>
2024-05-21 19:14:04 +0530

    course project setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,12 @@
 import { Component, ElementRef, ViewChild, viewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
-import { ParentComponent } from './BindMethod/parent/parent.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule, ParentComponent],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'ng-learn';
-  disable = true;
-  twoway: String = 'hello';
-
-  /**
-   * @abstract local reference template
-   */
-  @ViewChild('inputVal', { static: true }) value: any;
-
-  // console.log(value)
-
-  // two way binding
-  onAddOne(serverData: { name: string }) {
-    console.log('add one', serverData.name);
-  }
-  onAddTwo() {
-    console.log(this.value.nativeElement.value);
-    console.log('add two');
-  }
-}
+export class AppComponent {}
